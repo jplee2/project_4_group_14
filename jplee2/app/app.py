07ad50ac1f6e78315ee75edafe673e5d560ab2ad
James Lee <james.p.lee0694@gmail.com>
2024-09-30 19:15:32 -0500

    project update

--- jplee2/app/app.py
+++ jplee2/app/app.py
@@ -16,37 +16,20 @@ modelHelper = ModelHelper()
 
 # HTML ROUTES
 @app.route("/")
-def index():
+def home():
+    # Return template and data
     return render_template("home.html")
 
-
-# Notes: Live serving model deployment. This listens for requests of user-provided info from an
-# html form. Big Idea: user inputs sent to backend and then you need a function (modelHelper)
-# and you'll see modelHelper takes in user inputs and makes the prediction
-@app.route("/makePredictions", methods=["POST"])
-def make_predictions():
-    content = request.json["data"]
-    print(content)
-
-    # parse
-    sex_flag = content["sex_flag"]
-    age = float(content["age"])
-    fare = float(content["fare"])
-    familySize = int(content["familySize"])
-    p_class = int(content["p_class"])
-    embarked = content["embarked"]
-    has_cabin = bool(int(content["has_cabin"]))
-
-    preds = modelHelper.makePredictions(sex_flag, age, fare, familySize, p_class, embarked, has_cabin)
-    return(jsonify({"ok": True, "prediction": str(preds)}))
-
+@app.route("/predict")
+def index():
+    return render_template("index.html")
 
 @app.route("/dashboard1")
-def dashboard():
+def dashboard1():
     return render_template("tableau_1.html")
 
 @app.route("/dashboard2")
-def dashboard():
+def dashboard2():
     return render_template("tableau_2.html")
 
 @app.route("/about_us")
@@ -57,6 +40,34 @@ def about_us():
 def work_cited():
     return render_template("workcited.html")
 
+# @app.route("/makePredictions", methods=["POST"])
+# def make_predictions():
+#     content = request.json["data"]
+#     print(content)
+
+#     # parse
+#     sex_flag = content["sex_flag"]
+#     age = float(content["age"])
+#     fare = float(content["fare"])
+#     familySize = int(content["familySize"])
+#     p_class = int(content["p_class"])
+#     embarked = content["embarked"]
+#     has_cabin = bool(int(content["has_cabin"]))
+
+#     preds = modelHelper.makePredictions(sex_flag, age, fare, familySize, p_class, embarked, has_cabin)
+#     return(jsonify({"ok": True, "prediction": str(preds)}))
+
+# @app.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     return r
 
 # Run the App
 if __name__ == '__main__':
